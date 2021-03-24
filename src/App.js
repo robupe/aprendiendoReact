d3fc81a81d8dbc8c7aafa6d0b18bbe71545a6cb4
robupe <74590539+robupe@users.noreply.github.com>
2021-03-24 14:12:47 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Aprendiendo React</h1>
+      <h2>APP con React</h2>
+    </div>
+  );
+}
